166c5a11376695bd898c31eec7ec055c97a3739f
Simon T. <simtho0624@gmail.com>
2025-06-05 16:47:15 +0200

    Add connection to db

--- app.js
+++ app.js
@@ -1,9 +1,48 @@
 const express = require('express');
+const mysql = require('mysql2');
+const path = require('path');
+require('dotenv').config();
+
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req,res) => {
-    res.send('Hello from Express!')
+const db_host = process.env.DB_HOST;
+const db_user = process.env.DB_USER;
+const db_password = process.env.DB_PASSWORD;
+const db_database = process.env.DB_NAME;
+const db_table_name = process.env.DB_TABLE_NAME;
+
+console.log(`_____________${db_host}________`)
+
+// Mysql connection
+const db = mysql.createConnection({
+    host: db_host,
+    user: db_user,
+    password: db_password,
+    database: db_database
+});
+
+db.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to database');
+});
+
+// app.get('/', (req,res) => {
+//     res.send('Hello from Express!')
+// });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'html/index.html'));
+});
+
+app.get('/api/data', (req, res) => {
+    db.query(`SELECT * FROM ${db_table_name}`, (err, results) => {
+        if (err) {
+            console.error('Error fetching data:', err);
+            return res.status(500).json({ error: 'Database query failed' });
+        }
+        res.json(results);
+    });
 });
 
 app.listen(PORT, () => {
